995547e2797cd73abdc0f421a7f60890ea4a0649
preyasu <preyasu.official@gmail.com>
2023-01-14 01:46:51 +0530

    security commit

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import praw
+import creds
 
 #credentials
-client_id = 'kSHCtP4MvLjhwjj3P7BFcg'
-client_secret = 'Wbqh3cno-jQ4JJhcpv6QR5N07y51jQ'
-user_agent = 'preyasu'
-username = 'preyasu'
-password = 'redpass1234'
+client_id = creds.client_id
+client_secret = creds.client_secret
+user_agent = creds.user_agent
+username = creds.username
+password = creds.password
 
 #initialising an instance
 reddit = praw.Reddit(client_id= client_id, client_secret= client_secret, user_agent= user_agent, username= username, password= password)
