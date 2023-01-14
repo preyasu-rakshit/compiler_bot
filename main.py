8d239ae4f8252a1e14b1d761fa5572ed8db30d6a
preyasu <preyasu.official@gmail.com>
2023-01-15 00:10:50 +0530

    just experimenting

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import praw
 import creds
 import requests
+import logging
+from telegram import Update 
+from telegram.ext import *
+
+#logging for the py-telegram-bot library
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
 
 #credentials
 client_id = creds.client_id
@@ -12,6 +22,7 @@ password = creds.password
 
 def download_posts(list_of_posts):
     '''Give list of posts to be downloaded'''
+    
     name = 0
     for i in list_of_posts:
         ext = i.url.split('.')[-1]
@@ -30,15 +41,35 @@ def sort_posts(raw):
 
     return posts
 
-def main():
-   
-    #initialising an instance
+def update_database(context):
+    'context is a dummy variable for the job scheduler'
+    
+     #initialising an instance
     reddit = praw.Reddit(client_id= client_id, client_secret= client_secret, user_agent= user_agent, username= username, password= password)
     sub = input("Enter the Subreddit you want to scrape: ")
     subred = reddit.subreddit(sub)
     unsorted_posts = subred.top(limit= 10, time_filter='day')
     posts = sort_posts(unsorted_posts)
     download_posts(posts)
+
+#bot commands
+def start(update, context):
+    user_id = update.effective_chat.id
+    msg = '''Hi! I will now brighten up everyday with motivating quotes, wholesome memes, cute cat/dog videos etc ;)'''
+    update.message.reply_text(msg)
+    
+    with open('users.id', 'a+') as f:
+        f.write(str(user_id))
+
+def main():
+    # updater = Updater("1677058970:AAELb-SlS4Y2jZGfW2NoeAvfnbkcP0W-Tms")
+    # dispatcher = updater.dispatcher
+    # j = updater.job_queue
+
+    application = Application.builder().token('5938253500:AAG48XzepJzhAxue4jZ796jw3WI7kDSwg6g').build()
+    application.add_handler(CommandHandler('start', start))
+    application.run_polling()
+    application.idle()
 
 
 if __name__ == "__main__":
