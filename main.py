527220c367dfd23fe8fe2aca81a902ca5ffe32c8
preyasu <preyasu.official@gmail.com>
2023-01-14 22:55:15 +0530

    perfected a rough sorting system

--- main.py
+++ main.py
@@ -14,14 +14,21 @@ def download_posts(list_of_posts):
     '''Give list of posts to be downloaded'''
     name = 0
     for i in list_of_posts:
-        if 'i.redd.it' in i.url:
-            ext = i.url.split('.')[-1]
-            post = requests.get(i.url)
-            print(ext)
-            with open(f'fetched_media/{name}.{ext}', 'wb+') as file:
-                file.write(post.content)
-            name += 1
+        ext = i.url.split('.')[-1]
+        post = requests.get(i.url)
+        print(ext)
+        with open(f'fetched_media/{name}.{ext}', 'wb+') as file:
+            file.write(post.content)
+        name += 1
 
+
+def sort_posts(raw):
+    posts = []
+    for post in raw:
+        if 'i.redd.it' in post.url:
+            posts.append(post)
+
+    return posts
 
 def main():
    
@@ -29,8 +36,9 @@ def main():
     reddit = praw.Reddit(client_id= client_id, client_secret= client_secret, user_agent= user_agent, username= username, password= password)
     sub = input("Enter the Subreddit you want to scrape: ")
     subred = reddit.subreddit(sub)
-    hot = subred.top(limit= 10)
-    download_posts(hot)
+    unsorted_posts = subred.top(limit= 10, time_filter='day')
+    posts = sort_posts(unsorted_posts)
+    download_posts(posts)
 
 
 if __name__ == "__main__":
