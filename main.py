57b5b0fea44e1913778cdeb8d0693305b4e688d0
preyasu <preyasu.official@gmail.com>
2023-01-14 22:06:49 +0530

    sorted filter system

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import praw
 import creds
+import requests
 
 #credentials
 client_id = creds.client_id
@@ -8,12 +9,29 @@ user_agent = creds.user_agent
 username = creds.username
 password = creds.password
 
-#initialising an instance
-reddit = praw.Reddit(client_id= client_id, client_secret= client_secret, user_agent= user_agent, username= username, password= password)
 
-subred = reddit.subreddit('shitposting')
+def download_posts(list_of_posts):
+    '''Give list of posts to be downloaded'''
+    name = 0
+    for i in list_of_posts:
+        if 'i.redd.it' in i.url:
+            ext = i.url.split('.')[-1]
+            post = requests.get(i.url)
+            print(ext)
+            with open(f'fetched_media/{name}.{ext}', 'wb+') as file:
+                file.write(post.content)
+            name += 1
 
-top = subred.top(limit= 10, time_filter= 'all')
-for i in top:
-    print(i.url)
 
+def main():
+   
+    #initialising an instance
+    reddit = praw.Reddit(client_id= client_id, client_secret= client_secret, user_agent= user_agent, username= username, password= password)
+    sub = input("Enter the Subreddit you want to scrape: ")
+    subred = reddit.subreddit(sub)
+    hot = subred.top(limit= 10)
+    download_posts(hot)
+
+
+if __name__ == "__main__":
+    main()
