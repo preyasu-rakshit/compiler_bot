e6053f2bd9ef1b3f3a812cbfd2d290e2b4913cce
preyasu <preyasu.official@gmail.com>
2023-01-15 06:07:19 +0530

    telegram bot started

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import praw
 import creds
 import requests
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, JobQueue
+import random
+
 
 #credentials
 client_id = creds.client_id
@@ -8,6 +13,11 @@ client_secret = creds.client_secret
 user_agent = creds.user_agent
 username = creds.username
 password = creds.password
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
 
 def download_posts(list_of_posts):
@@ -20,6 +30,7 @@ def download_posts(list_of_posts):
         with open(f'fetched_media/{name}.{ext}', 'wb+') as file:
             file.write(post.content)
         name += 1
+        print(i.url)
 
 
 def sort_posts(raw):
@@ -30,16 +41,35 @@ def sort_posts(raw):
 
     return posts
 
-def main():
-   
-    #initialising an instance
+def update_posts():
+    global posts
+    #initialising a praw instance
     reddit = praw.Reddit(client_id= client_id, client_secret= client_secret, user_agent= user_agent, username= username, password= password)
     sub = input("Enter the Subreddit you want to scrape: ")
     subred = reddit.subreddit(sub)
     unsorted_posts = subred.top(limit= 10, time_filter='day')
     posts = sort_posts(unsorted_posts)
-    download_posts(posts)
+
+async def test(a):
+    print('jjj')
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_photo(context._chat_id, random.choice(posts).url)
+
+
+def main():
+    # update_posts()
+    application = ApplicationBuilder().token('5938253500:AAG48XzepJzhAxue4jZ796jw3WI7kDSwg6g').build()
+    application.job_queue.run_repeating(test, 1)
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+
+    application.run_polling()
+    
 
 
 if __name__ == "__main__":
-    main()
+    main()  
